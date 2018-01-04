1ab409dd9ddfa2f5bff12255cd279d68a040bab8
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-01-04 13:45:44 -0500

    finished front end development of the website

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RandomQuoteGenerator
 Simple Random Quote Generator 
+
+This is a simple random quote generator that uses the Twitter API to retrieve and display a new quote.
---
+++ main.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+	// declare colours to be used
+	var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c',
+					'#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99",
+					"#77B1A9", "#73A857"];
+
+	// get html elements as variables
+	var newQuoteBtn = $("#new-quote-btn");
+	var quote = $("#quote");
+	var author = $("#author");
+	var body = $("body");
+	var colorCounter = 0;
+
+	// set default bg colour for body
+	body.css("background-color", colors[colorCounter]);
+
+	// change colour of background and btn each time newQuoteBtn is click
+	// define event handler
+	function changeBgBtnColour(){
+		colorCounter = colorCounter + 1;
+		// change bg colours of body and btn
+		newQuoteBtn.css("background-color", colors[colorCounter]);
+		body.css("background-color", colors[colorCounter]);
+		// check if last colour was reached
+		if (colorCounter === colors.length - 1) {
+			colorCounter = -1;
+		}
+	}
+
+	// add event listener to button
+	newQuoteBtn.on("click", changeBgBtnColour);
+
+	// get new quote from Twitter API each time new quote btn is clicked
+});
